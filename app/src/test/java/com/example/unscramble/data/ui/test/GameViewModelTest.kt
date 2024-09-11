f253b5e59d7dad05a9d96a2db0a07f73fbb8e55f
Fleur41 <Muriithikings456@gmail.com>
2024-09-11 13:49:53 +0300

    Boundary  path

--- app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
+++ app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
@@ -6,24 +6,28 @@ import com.example.unscramble.ui.GameViewModel
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
+import org.junit.Assert.assertNotEquals
 import org.junit.Test
 
 class GameViewModelTest {
     private val viewModel = GameViewModel()
 
     @Test
-    fun gameViewModel_IncorrectGuess_ErrorFlagSet() {
-        // Given an incorrect word as input
-        val incorrectPlayerWord = "and"
 
-        viewModel.updateUserGuess(incorrectPlayerWord)
-        viewModel.checkUserGuess()
+    fun gameViewModel_Initialization_FirstWordLoaded() {
+        val gameUiState = viewModel.uiState.value
+        val unScrambledWord = getUnscrambledWord(gameUiState.currentScrambledWord)
 
-        val currentGameUiState = viewModel.uiState.value
-        // Assert that score is unchanged
-        assertEquals(0, currentGameUiState.score)
-        // Assert that checkUserGuess() method updates isGuessedWordWrong correctly
-        assertTrue(currentGameUiState.isGuessedWordWrong)
+        // Assert that current word is scrambled.
+        assertNotEquals(unScrambledWord, gameUiState.currentScrambledWord)
+        // Assert that current word count is set to 1.
+        assertTrue(gameUiState.currentWordCount == 1)
+        // Assert that initially the score is 0.
+        assertTrue(gameUiState.score == 0)
+        // Assert that the wrong word guessed is false.
+        assertFalse(gameUiState.isGuessedWordWrong)
+        // Assert that game is not over.
+        assertFalse(gameUiState.isGameOver)
     }
     companion object {
         private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
