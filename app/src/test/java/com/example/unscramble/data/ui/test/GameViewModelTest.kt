366b213026a197c460004599aef1975f1a7bcc39
Fleur41 <Muriithikings456@gmail.com>
2024-09-11 13:55:29 +0300

    Test instance lifecycle overview

--- app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
+++ app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
@@ -1,5 +1,6 @@
 package com.example.unscramble.data.ui.test
 
+import com.example.unscramble.data.MAX_NO_OF_WORDS
 import com.example.unscramble.data.SCORE_INCREASE
 import com.example.unscramble.data.getUnscrambledWord
 import com.example.unscramble.ui.GameViewModel
@@ -13,21 +14,23 @@ class GameViewModelTest {
     private val viewModel = GameViewModel()
 
     @Test
-
-    fun gameViewModel_Initialization_FirstWordLoaded() {
-        val gameUiState = viewModel.uiState.value
-        val unScrambledWord = getUnscrambledWord(gameUiState.currentScrambledWord)
-
-        // Assert that current word is scrambled.
-        assertNotEquals(unScrambledWord, gameUiState.currentScrambledWord)
-        // Assert that current word count is set to 1.
-        assertTrue(gameUiState.currentWordCount == 1)
-        // Assert that initially the score is 0.
-        assertTrue(gameUiState.score == 0)
-        // Assert that the wrong word guessed is false.
-        assertFalse(gameUiState.isGuessedWordWrong)
-        // Assert that game is not over.
-        assertFalse(gameUiState.isGameOver)
+    fun gameViewModel_AllWordsGuessed_UiStateUpdatedCorrectly() {
+        var expectedScore = 0
+        var currentGameUiState = viewModel.uiState.value
+        var correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+        repeat(MAX_NO_OF_WORDS) {
+            expectedScore += SCORE_INCREASE
+            viewModel.updateUserGuess(correctPlayerWord)
+            viewModel.checkUserGuess()
+            currentGameUiState = viewModel.uiState.value
+            correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+            // Assert that after each correct answer, score is updated correctly.
+            assertEquals(expectedScore, currentGameUiState.score)
+        }
+        // Assert that after all questions are answered, the current word count is up-to-date.
+        assertEquals(MAX_NO_OF_WORDS, currentGameUiState.currentWordCount)
+        // Assert that after 10 questions are answered, the game is over.
+        assertTrue(currentGameUiState.isGameOver)
     }
     companion object {
         private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
