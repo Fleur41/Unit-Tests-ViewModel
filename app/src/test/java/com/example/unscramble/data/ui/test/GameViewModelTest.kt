0a5e245228b285e35fe3e410e7c2bdda51172d9b
Fleur41 <Muriithikings456@gmail.com>
2024-09-11 13:07:35 +0300

    1st commit

---
+++ app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
@@ -0,0 +1,28 @@
+package com.example.unscramble.data.ui.test
+
+import com.example.unscramble.data.getUnscrambledWord
+import com.example.unscramble.ui.GameViewModel
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
+import org.junit.Test
+
+class GameViewModelTest {
+    private val viewModel = GameViewModel()
+
+    @Test
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset(){
+        var currentGameUiState = viewModel.uiState.value
+        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+
+        viewModel.updateUserGuess(correctPlayerWord)
+        viewModel.checkUserGuess()
+
+
+        currentGameUiState = viewModel.uiState.value
+        // Assert that checkUserGuess() method updates isGuessedWordWrong is updated correctly.
+        assertFalse(currentGameUiState.isGuessedWordWrong)
+        // Assert that score is updated correctly.
+        assertEquals(20, currentGameUiState.score)
+
+    }
+}
